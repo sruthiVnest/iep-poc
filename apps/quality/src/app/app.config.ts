fde71d9e04d23402f43517dfbaf01bb214dd8186
Sruthi.V <Sruthi.V@nestgroup.net>
2025-06-23 14:50:54 +0530

    import changes

--- apps/quality/src/app/app.config.ts
+++ apps/quality/src/app/app.config.ts
@@ -1,7 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes),provideHttpClient(),importProvidersFrom(BrowserAnimationsModule)],
 };
