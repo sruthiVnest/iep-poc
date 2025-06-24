078b71fe5c2865f27b5a9fbf2a9c36f552e44919
Sruthi.V <Sruthi.V@nestgroup.net>
2025-06-24 14:51:29 +0530

    dovsbuy title bar template change

--- apps/operations/src/app/pages/dovsbuy/dovsbuy.component.ts
+++ apps/operations/src/app/pages/dovsbuy/dovsbuy.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedUiIepGridComponent } from '@shared-ui/iep-grid';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'copilot-iep-nx-dovsbuy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SharedUiIepGridComponent],
   templateUrl: './dovsbuy.component.html',
   styleUrl: './dovsbuy.component.scss',
 })
-export class DovsbuyComponent {}
+export class DovsbuyComponent {
+public data: any = [];
+ private dataService = inject(DataService);
+   public gridView: any[] = [];
+  public mapView: any[] = [];
+    public ngOnInit(): void {
+    this.dataService.getGridData().subscribe((data) => {
+      this.data = data.data?.activities || [];
+      this.gridView = this.data.map((el: any) =>
+        Object.fromEntries(
+          Object.entries(el).map(([key, value]) => [
+            key.replace(/\s+/g, ''),
+            value,
+          ])
+        )
+      );
+      if (this.data) {
+        this.mapView = Object.keys(this.data[0]).map((key) => ({
+          field: key.replace(/\s+/g, ''),
+          title: key.replace('_', ' ').toUpperCase(),
+        }));
+      }
+    });
+  }
+}
--- apps/operations/src/app/pages/operation-dashboard/operation-dashboard.component.ts
+++ apps/operations/src/app/pages/operation-dashboard/operation-dashboard.component.ts
@@ -5,25 +5,34 @@ import { CommonModule } from '@angular/common';
 import { KENDO_DIALOG } from '@progress/kendo-angular-dialog';
 import { DovsbuyComponent } from '../dovsbuy/dovsbuy.component';
 import { SharedUiIepFilterContractComponent } from '@shared-ui/iep-filter-contract';
+import { FormsModule } from '@angular/forms';
+import { KENDO_INPUTS } from '@progress/kendo-angular-inputs';
 @Component({
   selector: 'app-operation-dashboard',
   standalone: true,
   templateUrl: './operation-dashboard.component.html',
   styleUrls: ['./operation-dashboard.component.scss'],
-  imports: [SharedUiIepFilterContractComponent, TabStripModule, IspoComponent, CommonModule, KENDO_DIALOG, DovsbuyComponent],
+  imports: [SharedUiIepFilterContractComponent, TabStripModule, IspoComponent, CommonModule, KENDO_DIALOG,KENDO_INPUTS, DovsbuyComponent,FormsModule],
 })
 export class OperationDashboardComponent {
   lastUpdated = '2025-06-03 10:00 AM';
   completion = 72; // Example percentage
   dashboard_expand = false;
   public doVsBuyDialogOpen = false;
-
+public searchValue: string = '';
   ngOnInit() {
       window.addEventListener('filterProjectsCollapse', (event: any) => {
       this.dashboard_expand = event.detail.collapsed;
     });
   }
-
+ public onFilter(value: string): void {
+    const inputValue = value.toLowerCase();
+   
+    if (!inputValue) {
+      return;
+    }
+  
+  }
   openDoVsBuyDialog() {
     this.doVsBuyDialogOpen = true;
   }
