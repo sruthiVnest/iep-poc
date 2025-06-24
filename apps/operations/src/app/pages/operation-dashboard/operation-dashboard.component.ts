1cfb85832fd919f0623909200fab6e3b3c2c8f3d
Sruthi.V <Sruthi.V@nestgroup.net>
2025-06-24 15:28:41 +0530

    dovsbuy title bar template css change

--- apps/operations/src/app/pages/operation-dashboard/operation-dashboard.component.ts
+++ apps/operations/src/app/pages/operation-dashboard/operation-dashboard.component.ts
@@ -7,16 +7,45 @@ import { DovsbuyComponent } from '../dovsbuy/dovsbuy.component';
 import { SharedUiIepFilterContractComponent } from '@shared-ui/iep-filter-contract';
 import { FormsModule } from '@angular/forms';
 import { KENDO_INPUTS } from '@progress/kendo-angular-inputs';
+import {
+  KENDO_PROGRESSBARS,
+  ProgressColor,
+} from "@progress/kendo-angular-progressbar";
 @Component({
   selector: 'app-operation-dashboard',
   standalone: true,
   templateUrl: './operation-dashboard.component.html',
   styleUrls: ['./operation-dashboard.component.scss'],
-  imports: [SharedUiIepFilterContractComponent, TabStripModule, IspoComponent, CommonModule, KENDO_DIALOG,KENDO_INPUTS, DovsbuyComponent,FormsModule],
+  imports: [SharedUiIepFilterContractComponent, TabStripModule, IspoComponent, CommonModule,
+     KENDO_DIALOG,KENDO_INPUTS, DovsbuyComponent,FormsModule, KENDO_PROGRESSBARS],
 })
 export class OperationDashboardComponent {
   lastUpdated = '2025-06-03 10:00 AM';
   completion = 72; // Example percentage
+
+  public animation = true;
+  public colors: ProgressColor[] = [
+    {
+      from: 0,
+      to: 25,
+      color: "#F42E17",
+    },
+    {
+      from: 25,
+      to: 50,
+      color: "#F8DE7E",
+    },
+    {
+      from: 50,
+      to: 75,
+      color: "#F2E349",
+    },
+    {
+      from: 75,
+      to: 100,
+      color: "#4A9E24",
+    },
+  ];
   dashboard_expand = false;
   public doVsBuyDialogOpen = false;
 public searchValue: string = '';
